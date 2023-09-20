a89201bfc566dd6e17ce48755b9c418f9b217ee2
Luofan Chen <clfbbn@gmail.com>
2023-09-20 19:57:27 +0800

    qcom: Import changes from sony copyleft release
    Change-Id: Ie4a5007f6cfecdcb25eac348495597a7c4ba3eee

--- qcom/camera/config/kalama.mk
+++ qcom/camera/config/kalama.mk
@@ -1,4 +1,17 @@
 dtbo-$(CONFIG_ARCH_KALAMA) := kalama-camera.dtbo
+
+SOMC_TARGET_PRODUCT := pdx234
+SOMC_TARGET_OPERATOR := generic
+SOMC_PLATFORM := yodo
+
+ifeq ($(SOMC_PLATFORM), yodo)
+
+ifeq ($(SOMC_TARGET_PRODUCT),pdx234)
+dtbo-$(CONFIG_ARCH_KALAMA)		+= kalama-camera-sensor-pdx234.dtbo
+endif
+
+else
+
 dtbo-$(CONFIG_ARCH_KALAMA) += kalama-camera-sensor-cdp.dtbo \
 								kalama-camera-sensor-mtp.dtbo \
 								kalama-camera-sensor-qrd.dtbo \
@@ -9,3 +22,5 @@ dtbo-$(CONFIG_ARCH_KALAMA) += kalama-camera-sensor-cdp.dtbo \
 dtbo-$(CONFIG_ARCH_CROW) += crow-camera.dtbo \
 				crow-camera-sensor-idp.dtbo \
 				crow-camera-sensor-qrd.dtbo
+
+endif
